4ebc9660b8eda70a20938be31067f544a53fc632
evengu <evengu@stud.ntnu.no>
2020-10-01 15:33:14 +0200

    Comments

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ skb = SelectKBest(chi2, k=5)
 skb.fit(X, y)
 X_new = skb.transform(X)
 # With feature selection, we get
-# Index(['bruises?', 'gill-size', 'gill-color', 'stalk-root', 'ring-type'], dtype='object')
+# odor_f', 'odor_n', 'gill-color_b', 'stalk-surface-above-ring_k', 'stalk-surface-below-ring_k'
 print(X.iloc[:, skb.get_support(indices=True)].columns)
 
 # We then run PCA on the same dataset
@@ -33,11 +33,11 @@ pcaDF = pd.DataFrame(pca.components_, columns=data_scaled.columns)
 maxValues = pcaDF.idxmax(axis=1)
 print("Max:\n%s" % maxValues)
 # And get
-# 0    spore-print-color
-# 1         gill-spacing
-# 2           population
-# 3          ring-number
-# 4          stalk-shape
+# 0    stalk-surface-above-ring_k
+# 1                  stalk-root_?
+# 2             gill-attachment_a
+# 3                     habitat_g
+# 4                        odor_m
 
-# There seems to be no overlap when I do it this way. If I do the get_dummies version, odor
+# There seems to be no overlap when I do it this way. If I do the lambda version, odor
 # and stalk-surface-above-ring is in both sets
